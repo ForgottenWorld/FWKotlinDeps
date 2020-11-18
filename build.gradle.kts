b8b2dd8e0f356634520b5ab757995e3b17e072cf
giacomo <giacomozama@hotmail.it>
2020-11-18 22:26:45 +0100

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
     `maven-publish`
     kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
@@ -15,15 +18,16 @@ repositories {
 
 dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT")
+    implementation("com.github.ForgottenWorld.MCCoroutine:mccoroutine-bukkit-api:v0.0.7")
     implementation("com.github.ForgottenWorld.MCCoroutine:mccoroutine-bukkit-core:v0.0.7")
     implementation("io.github.rybalkinsd:kohttp:0.12.0")
-    implementation("org.jetbrains.kotlin:kotlin-maven-serialization:1.4.10")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
 }
 
 group = "it.forgottenworld"
-version = "0.0.4"
+version = "0.0.5"
 description = "FWKotlinDeps"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
@@ -34,5 +38,13 @@ publishing {
 }
 
 tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
     options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
