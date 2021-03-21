a3da60ce54be6eee58bfff17006477bde6298261
Giacomo Zama <giacomozama@hotmail.it>
2021-03-21 22:37:31 +0100

    bumped versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     maven
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.serialization") version "1.4.21"
+    kotlin("jvm") version "1.4.31"
+    kotlin("plugin.serialization") version "1.4.31"
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("com.github.ben-manes.versions") version "0.36.0"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 repositories {
@@ -19,17 +19,15 @@ repositories {
 dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
     implementation("io.github.rybalkinsd:kohttp:0.12.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.21")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:0.0.6")
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:0.0.6")
-    implementation("com.charleskorn.kaml:kaml:0.26.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.31")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+    implementation("com.charleskorn.kaml:kaml:0.28.3")
     implementation("com.github.BrunoSilvaFreire:Skedule:0.1.3")
 }
 
 group = "it.forgottenworld"
-version = "0.0.8"
+version = "0.0.9"
 description = "FWKotlinDeps"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
