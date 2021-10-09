8b70cf0c6f42da359c424b7bb1716dfaa4210b94
Giacomo Zama <giacomozama@hotmail.it>
2021-10-09 17:27:30 +0200

    bumped versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.4.32"
-    kotlin("plugin.serialization") version "1.4.32"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("com.github.ben-manes.versions") version "0.38.0"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 repositories {
@@ -15,16 +15,15 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
     implementation("io.github.rybalkinsd:kohttp:0.12.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.32")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.32")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
-    implementation("com.charleskorn.kaml:kaml:0.30.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.31")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("com.charleskorn.kaml:kaml:0.36.0")
     implementation("com.github.BrunoSilvaFreire:Skedule:0.1.3")
 
-    val exposedVersion = "0.30.2"
+    val exposedVersion = "0.35.1"
 
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
@@ -33,18 +32,18 @@ dependencies {
 }
 
 group = "it.forgottenworld"
-version = "0.1.0"
+version = "0.2.0"
 description = "FWKotlinDeps"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_16
 
 tasks.withType<JavaCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
+    sourceCompatibility = "16"
+    targetCompatibility = "16"
     options.encoding = "UTF-8"
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "16"
     }
 }
